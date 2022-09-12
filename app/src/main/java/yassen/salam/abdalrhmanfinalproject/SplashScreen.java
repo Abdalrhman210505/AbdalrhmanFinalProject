f99f9e6bfdc40d3d017286c400ba9b34d7533afd
doniataskman <doniahammud@gmail.com>
2022-09-12 14:11:02 +0300

    splash screen code finished need to be tested

--- app/src/main/java/yassen/salam/abdalrhmanfinalproject/SplashScreen.java
+++ app/src/main/java/yassen/salam/abdalrhmanfinalproject/SplashScreen.java
@@ -1,8 +1,12 @@
 package yassen.salam.abdalrhmanfinalproject;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //start next activity (screen) automaically afer period of time فتح الشاشة التالية تلقائيا بعد فترة من الزمن
+        Handler h=new Handler();
+        Runnable R=new Runnable() {
+            @Override
+            public void run() {
+                //to open new screen from current to next screen
+                Intent i=new Intent(SplashScreen.this,SignIn.class);
+                startActivity(i);
+                //to close current activity
+                finish();
+
+
+            }
+        };
+        h.postDelayed(R,3000);
+
+
     }
 }
