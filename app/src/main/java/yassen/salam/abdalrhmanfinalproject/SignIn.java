625ad008d6c2e5473f02f83dbdc69732b474741d
doniataskman <doniahammud@gmail.com>
2022-09-15 11:36:14 +0300

    add sign up activity

--- app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignIn.java
+++ app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignIn.java
@@ -2,13 +2,35 @@ package yassen.salam.abdalrhmanfinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignIn extends AppCompatActivity {
+    private TextInputEditText etEmail;
+    private TextInputEditText etPassword;
+    private Button btnsignin;
+    private Button btnsignup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        etEmail=findViewById(R.id.etEmail);
+        etPassword=findViewById(R.id.etPassword);
+        btnsignin=findViewById(R.id.btnsignin);
+        btnsignup=findViewById(R.id.btnsignup);
+        btnsignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(SignIn.this,SignUp.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
--- app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignUp.java
+++ app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignUp.java
@@ -3,12 +3,23 @@ package yassen.salam.abdalrhmanfinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class SignUp extends AppCompatActivity {
-
+    private TextInputEditText hEmail;
+    private TextInputEditText hPassword;
+    private TextInputEditText hConfirm;
+    private Button btnsave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        hEmail=findViewById(R.id.hEmail);
+        hPassword=findViewById(R.id.hPass);
+        hConfirm=findViewById(R.id.hConfirm);
+        btnsave=findViewById(R.id.btnsave);
     }
+
 }
