bdf25ff2db39a24b3d6843bebcc8dfa2c0f5c17c
doniataskman <doniahammud@gmail.com>
2022-09-21 14:59:59 +0300

    signup fire base added

--- app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignUpActivity.java
+++ app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignUpActivity.java
@@ -3,9 +3,11 @@ package yassen.salam.abdalrhmanfinalproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUpActivity extends AppCompatActivity {
     private TextInputEditText hEmail;
@@ -20,6 +22,31 @@ public class SignUpActivity extends AppCompatActivity {
         hPassword=findViewById(R.id.hPass);
         hConfirm=findViewById(R.id.hConfirm);
         btnsave=findViewById(R.id.btnsave);
+    btnsave.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            checkandsave();
+        }
+    });
     }
 
+    private void checkandsave() {
+        String email = hEmail.getText().toString();
+        String password = hPassword.getText().toString();
+        String confirm = hConfirm.getText().toString();
+
+        boolean isOk=true;
+        if (email.length()*password.length()*confirm.length()==0){
+            hEmail.setError("one of the files are emtpy");
+            isOk=false;
+
+        }
+        if (password.equals(confirm)==false){
+            hConfirm.setError("password doesnt match");
+            isOk=false;
+
+        }
+        if (isOk){
+            FirebaseAuth auth=FirebaseAuth.getInstance();
+        }
 }
