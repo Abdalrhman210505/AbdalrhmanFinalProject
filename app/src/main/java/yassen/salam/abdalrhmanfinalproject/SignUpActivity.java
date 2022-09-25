125c1da4832d750f5dda53bc6b3260d94a1e6076
doniataskman <doniahammud@gmail.com>
2022-09-25 09:17:31 +0300

    twthee8

--- app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignUpActivity.java
+++ app/src/main/java/yassen/salam/abdalrhmanfinalproject/SignUpActivity.java
@@ -19,51 +19,54 @@ public class SignUpActivity extends AppCompatActivity {
     private TextInputEditText hPassword;
     private TextInputEditText hConfirm;
     private Button btnsave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        hEmail=findViewById(R.id.hEmail);
-        hPassword=findViewById(R.id.hPass);
-        hConfirm=findViewById(R.id.hConfirm);
-        btnsave=findViewById(R.id.btnsave);
-    btnsave.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            checkandsave();
-        }
-    });
+        hEmail = findViewById(R.id.hEmail);
+        hPassword = findViewById(R.id.hPass);
+        hConfirm = findViewById(R.id.hConfirm);
+        btnsave = findViewById(R.id.btnsave);
+        btnsave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkandsave();
+            }
+        });
     }
 
-    private void checkandsave() {
+    private void checkandsave() {//تضع الايميل وكلمة المرور في متغيرات من نوع سترينج وتبدا بفحص ان كانت ملائمة والكلمات المرور مطابقة\
+
         String email = hEmail.getText().toString();
         String password = hPassword.getText().toString();
         String confirm = hConfirm.getText().toString();
 
-        boolean isOk=true;
-        if (email.length()*password.length()*confirm.length()==0){
+        boolean isOk = true;//we added this para if all of condtions (email/pass) are ok it will remain true to change activity
+        if (email.length() * password.length() * confirm.length() == 0) {
             hEmail.setError("one of the files are emtpy");
-            isOk=false;
+            isOk = false;
 
         }
-        if (password.equals(confirm)==false){
+        if (password.equals(confirm) == false) {//if password and confirm password the same (match)
             hConfirm.setError("password doesnt match");
-            isOk=false;
+            isOk = false;
 
         }
-        if (isOk){
-            FirebaseAuth auth=FirebaseAuth.getInstance();
-            auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+        if (isOk) {// if (isok)==true that mean all condtions are ok so the code will send to firebase the email and password and authenaction it.
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
+                    if (task.isSuccessful()) {
                         Toast.makeText(SignUpActivity.this, "creation is successful", Toast.LENGTH_SHORT).show();
                         finish();
-                    }
-                    else {
-                        Toast.makeText(SignUpActivity.this, "creation field"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(SignUpActivity.this, "creation field" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 }
             });
         }
+    }
 }
+
