736f823aca8e725fe7a2d4b653b66c22a4a53f69
Abdalrhman210505 <abed.awad210@gmail.com>
2022-09-27 13:19:52 +0300

    add task class

---
+++ app/src/main/java/yassen/salam/abdalrhmanfinalproject/data/Task.java
@@ -0,0 +1,66 @@
+package yassen.salam.abdalrhmanfinalproject.data;
+
+public class Task {
+    private String key; //to identify the task by its own key
+    private String Title;
+    private String Subject;
+    private String TaskImportance; //to know about importance
+    private String Owner; //name of task owner
+
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public String getTaskImportance() {
+        return TaskImportance;
+    }
+
+    public void setTaskImportance(String taskImportance) {
+        TaskImportance = taskImportance;
+    }
+
+    public String getOwner() {
+        return Owner;
+    }
+
+    public void setOwner(String owner) {
+        Owner = owner;
+    }
+    @Override
+    public String toString() {
+        return "Task{" +
+                "key='" + key + '\'' +
+                ", Title='" + Title + '\'' +
+                ", Subject='" + Subject + '\'' +
+                ", TaskImportance='" + TaskImportance + '\'' +
+                ", Owner='" + Owner + '\'' +
+                '}';
+    }
+}
+
+
+public Task(){
+
+}
+
